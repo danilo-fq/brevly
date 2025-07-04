9cbd074efc07cccd0d4d6829a91dd7b456ab9c72
Danilo Queiroz <danilof.queiroz1@gmail.com>
2025-07-04 17:08:13 -0300

    fix(server): change method and fix message type errors

--- server/src/infra/routes/get-original-url.ts
+++ server/src/infra/routes/get-original-url.ts
@@ -2,29 +2,32 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
 import { NotFoundShortenedUrlError } from '../../app/errors/not-found-shortened-url-error'
 import { getOriginalUrl } from '../../app/functions/get-original-url'
-import { getShortenedUrlByName } from '../../app/functions/get-shortened-url-by-name'
 
 export const getOriginalUrlRoute: FastifyPluginAsyncZod = async app => {
-	app.delete(
+	app.get(
 		'/urls/:shortCodeUrl',
 		{
 			schema: {
-				summary: 'Delete a shortened URL',
+				summary: 'Get an original URL',
 				params: z.object({
 					shortCodeUrl: z
 						.string()
-						.min(4, 'URL encurtada deve ter no mínimo 4 caracteres.')
-						.max(10, 'URL encurtada deve ter no máximo 10 caracteres.')
-						.regex(
-							/^[a-z0-9-]+$/,
-							'URL encurtada deve conter apenas letras minúsculas, números e hífen.'
-						)
+						.min(4, { error: 'URL encurtada deve ter no mínimo 4 caracteres.' })
+						.max(10, { error: 'URL encurtada deve ter no máximo 10 caracteres.' })
+						.regex(/^[a-z0-9-]+$/, {
+							error: 'URL encurtada deve conter apenas letras minúsculas, números e hífen.',
+						})
 						.describe('The unique short code for the shortened URL'),
 				}),
+
 				response: {
-					204: {
-						description: 'Shortened URL deleted successfully',
-					},
+					200: z
+						.object({
+							originalUrl: z
+								.url()
+								.describe('The original URL that corresponds to the provided short code'),
+						})
+						.describe('The original URL that corresponds to the provided short code'),
 
 					404: z.object({
 						message: z.string().describe('No shortened URL was found for the provided short code'),
@@ -47,39 +50,3 @@ export const getOriginalUrlRoute: FastifyPluginAsyncZod = async app => {
 		}
 	)
 }
-
-// export const getOriginalUrlRoute: FastifyPluginAsyncZod = async app => {
-// 	app.get(
-// 		'/urls/:shortCodeUrl',
-// 		{
-// 			schema: {
-// 				summary: 'Redirect to the original URL using the short code',
-// 				params: {
-// 					shortCodeUrl: z
-// 						.string()
-// 						.min(4, 'URL encurtada deve ter no mínimo 4 caracteres.')
-// 						.max(10, 'URL encurtada deve ter no máximo 10 caracteres.')
-// 						.regex(
-// 							/^[a-z0-9-]+$/,
-// 							'URL encurtada deve conter apenas letras minúsculas, números e hífen.'
-// 						)
-// 						.describe('The unique short code for the shortened URL'),
-// 				},
-// 				response: {
-// 					302: {
-// 						description: 'The client was successfully redirected to the original URL.',
-// 					},
-
-// 					404: z.object({
-// 						message: z.string().describe('Original URL not found!'),
-// 					}),
-// 				},
-// 			},
-// 		},
-// 		async (req, reply) => {
-// 			const {} = req.params
-
-// 			return reply.status(302).redirect()
-// 		}
-// 	)
-// }
