9c30779f807020b29f8fa1383f6993029b825092
Danilo Queiroz <danilof.queiroz1@gmail.com>
2025-07-04 17:05:48 -0300

    feat(server): create get all shortened urls route, function and tests

---
+++ server/src/app/functions/get-all-shortened-urls.spec.ts
@@ -0,0 +1,35 @@
+import { beforeAll, describe, expect, it } from 'vitest'
+import { db } from '../../infra/db'
+import { schema } from '../../infra/db/schemas'
+import { createShortenedUrl } from './create-shortened-url'
+import { getAllShortenedUrls } from './get-all-shortened-urls'
+
+describe('Get all shortened URLs', () => {
+	beforeAll(async () => {
+		await db.delete(schema.urls)
+	})
+
+	it('should be able to get a list of shortened urls', async () => {
+		for (let i = 1; i <= 5; i += 1) {
+			await createShortenedUrl({
+				originalUrl: 'https://example.com',
+				shortCodeUrl: `short-${i}`,
+			})
+		}
+
+		const sut = await getAllShortenedUrls()
+
+		expect(sut).toHaveLength(5)
+		expect(sut).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({
+					id: expect.any(String),
+					originalUrl: expect.any(String),
+					shortCodeUrl: expect.any(String),
+					countViews: 0,
+					createdAt: expect.any(Date),
+				}),
+			])
+		)
+	})
+})
