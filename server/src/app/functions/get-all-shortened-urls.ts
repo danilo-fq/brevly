504d66dcf4907e626069b73b52d24cfbdc1a0754
Danilo Queiroz <danilof.queiroz1@gmail.com>
2025-07-07 08:53:46 -0300

    fix(server): return select directly

--- server/src/app/functions/get-all-shortened-urls.ts
+++ server/src/app/functions/get-all-shortened-urls.ts
@@ -1,13 +1,9 @@
-import { eq, sql } from 'drizzle-orm'
 import { db } from '../../infra/db'
 import { schema } from '../../infra/db/schemas'
-import { NotFoundShortenedUrlError } from '../errors/not-found-shortened-url-error'
 import type { CreateShortenedUrlOutput as ShortenedUrl } from './create-shortened-url'
 
 type GetAllShortenedUrlsOutput = ShortenedUrl[]
 
 export async function getAllShortenedUrls(): Promise<GetAllShortenedUrlsOutput> {
-	const shortenedUrls = await db.select().from(schema.urls)
-
-	return shortenedUrls
+	return db.select().from(schema.urls)
 }
