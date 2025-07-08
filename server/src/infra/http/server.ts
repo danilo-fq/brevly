9027b98e0a9f18717c586b5d9815a226638b72a9
Danilo Queiroz <danilof.queiroz1@gmail.com>
2025-07-08 12:51:03 -0300

    fix(server): include host on server.listen and logger of fastify server

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,6 +1,6 @@
 import { env } from '../../env'
 import { app } from './app'
 
-app.listen({ port: env.PORT }, () => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }, () => {
 	console.log(`Server is running at PORT ${env.PORT}!!!`)
 })
