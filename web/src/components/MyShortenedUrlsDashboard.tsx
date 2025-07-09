47dcdb06f2f1d8d85408e16aff49e568a6c3046b
Danilo Queiroz <danilof.queiroz1@gmail.com>
2025-07-09 14:54:41 -0300

    feat(web): create static homepage

---
+++ web/src/components/MyShortenedUrlsDashboard.tsx
@@ -0,0 +1,24 @@
+import { DownloadSimpleIcon, LinkIcon } from '@phosphor-icons/react'
+
+export function MyShortenedUrlsDashboard() {
+	return (
+		<div className="bg-white flex flex-col gap-4 justify-center p-6 rounded-lg w-full">
+			<header className="flex justify-between items-center">
+				<h2 className="text-lg text-gray-600">Meus Links</h2>
+				<button
+					className="bg-gray-200 disabled:bg-gray-200/50 disabled:text-gray-500/50 font-[620] flex gap-x-1.5 group items-center justify-between p-2 rounded-sm text-gray-500 text-sm"
+					type="button"
+					disabled={true}
+				>
+					<DownloadSimpleIcon weight="bold" size={16} />
+					Baixar CSV
+				</button>
+			</header>
+			<hr className="h-[1px] border-gray-200" />
+			<div className="flex flex-col gap-y-3 items-center justify-center py-4">
+				<LinkIcon className="text-gray-400" size={32} />
+				<p className="text-gray-500 text-xs">Ainda não existe links cadastrados</p>
+			</div>
+		</div>
+	)
+}
