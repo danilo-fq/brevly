b1e48ae6157aceca90655200b0aeb2bb674cd85c
Danilo Queiroz <danilof.queiroz1@gmail.com>
2025-07-10 21:57:40 -0300

    fix(web): add new Database local URL enviroment for local machine for using on migrates

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'drizzle-kit'
 import { env } from './src/env'
 
+if (!env.DATABASE_URL_LOCALHOST_MIGRATIONS) {
+	throw new Error('DATABASE_URL_LOCALHOST_MIGRATIONS is not defined in the environment variables.')
+}
+
 export default defineConfig({
 	out: './src/infra/db/migrations',
 	schema: './src/infra/db/schemas/*',
 	dialect: 'postgresql',
 	dbCredentials: {
-		url: env.DATABASE_URL,
+		url: env.DATABASE_URL_LOCALHOST_MIGRATIONS,
 	},
 })
